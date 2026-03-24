9b9a2f055b9188f5510f7a85f9e2db882faac777
Tchips46 <leo.out46@gmail.com>
2026-03-24 13:18:18 +0900

    feat(save): params value

--- src/utils/main/main-utils.ts
+++ src/utils/main/main-utils.ts
@@ -2,6 +2,6 @@ export const getStringParam = (value: any): string => {
   if (typeof value === "string") return `"${value}"`;
   if (typeof value === "number") return value.toString();
   if (typeof value === "boolean") return value ? "true" : "false";
-  if (typeof value === "object") return getStringParam(value);
+  if (typeof value === "object") return JSON.stringify(value);
   return "undefined";
 };
